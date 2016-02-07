92dd3babb0eee03ea9945f5267bd88af91233fcf
Kevin Blake <kblake@puzzlebox.co.uk>
2016-02-07 16:45:55 +0000

    Include scheduler

---
+++ run.py
@@ -0,0 +1,46 @@
+from energenie import switch_on, switch_off
+from time import sleep
+import ephem  
+import schedule
+import time
+from datetime import datetime, timedelta
+
+o=ephem.Observer()  
+o.lat='51.6550'  
+o.long='0.3957'  
+
+def turn_on_at_sunset():
+	s=ephem.Sun()  
+	s.compute()  
+
+	dtnow = datetime.now()
+
+	onAfter = datetime(dtnow.year, dtnow.month, dtnow.day, 17, 0, 0, 0)
+
+	sunset = ephem.localtime(o.next_setting(s));
+
+	turnOnTime = sunset;
+
+	if (sunset <= onAfter):
+		turnOnTime = onAfter;
+	
+	print "turning on at: " + turnOnTime.strftime("%Y-%m-%d %H:%M:%S")
+
+	schedule.every().day.at(turnOnTime.strftime("%H:%M")).do(turn_on_lamps_once)
+
+def turn_off_lamps():
+	switch_on(1)
+
+def turn_on_lamps_once():
+	switch_on(1)
+	return schedule.CancelJob
+
+turn_on_at_sunset();
+
+schedule.every().day.at('22:45').do(turn_off_lamps)
+schedule.every().day.at('7:45').do(turn_on_at_sunset)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
+
