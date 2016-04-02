5fc47bb760b7ebe153a0c0f366a850642fbb6a4d
Kevin Blake <kblake@puzzlebox.co.uk>
2016-04-02 21:48:08 +0100

    adjust for BST

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from energenie import switch_on, switch_off
 from time import sleep
+import pytz
 import ephem  
 import schedule
 import time
 from datetime import datetime, timedelta
+from dateutil.tz import tzlocal
 
 o=ephem.Observer()  
 o.lat='51.6550'  
@@ -15,8 +17,7 @@ def turn_on_at_sunset():
 
 	dtnow = datetime.now()
 
-	onAfter = datetime(dtnow.year, dtnow.month, dtnow.day, 18, 10, 0, 0)
-
+	onAfter = datetime(dtnow.year, dtnow.month, dtnow.day, 19, 10, 0, 0)
 	sunset = ephem.localtime(o.next_setting(s));
 
 	turnOnTime = sunset;
@@ -24,7 +25,7 @@ def turn_on_at_sunset():
 	if (sunset <= onAfter):
 		turnOnTime = onAfter;
 	
-	print "turning on at: " + turnOnTime.strftime("%Y-%m-%d %H:%M:%S")
+	print "turning on at: " + turnOnTime.strftime("%Y-%m-%d %H:%M:%S+%z")
 
 	schedule.every().day.at(turnOnTime.strftime("%H:%M")).do(turn_on_lamps_once)
 
@@ -40,7 +41,7 @@ def turn_on_lamps_once():
 
 turn_on_at_sunset();
 
-schedule.every().day.at('23:15').do(turn_off_lamps)
+schedule.every().day.at('22:45').do(turn_off_lamps)
 schedule.every().day.at('9:45').do(turn_on_at_sunset)
 schedule.every().day.at('7:15').do(turn_on_lamps)
 schedule.every().day.at('8:00').do(turn_on_lamps)
@@ -48,4 +49,5 @@ schedule.every().day.at('8:00').do(turn_on_lamps)
 while 1:
     schedule.run_pending()
     time.sleep(1)
+
 
